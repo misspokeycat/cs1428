5dfc07f6f11a61b2798d75f8c52d6e72fc57335c
pokemonmegaman <pokemonmegaman@yahoo.com>
2015-08-27 17:19:35 -0500

    Added prelab 01 file/initial commit

---
+++ Lab 1 Prelab/prelab_01.cpp
@@ -0,0 +1,36 @@
+// lab 1 practice 1
+
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+	// 1. make NAME a constant
+	string NAME = "Fred Flintstone"; 
+
+
+	// 2. Give in an appropriate data type for each of the 
+    //    following variables (replace underscores).
+	___ age = 38;
+
+	___ gpa = 1.5;
+
+	___ grade = 'D';
+
+	___ enrolled = true;
+
+	
+	cout << NAME << " is " << age << " years old. He has a " 
+	     << gpa << " GPA. His letter grade is a(n)" << grade << endl;
+	
+	// 3. write a cout statement that prints "Hello World" to the 
+	//    console using the following two variables.
+	string hello = "Hello";
+	string world = "World";
+	
+	
+	
+	
+
+	return 0;
+}
