d95c883a579f6bba312c33f170148f391f7386ad
Luke D'Alfonso <lukedalfonso@yahoo.com>
2015-08-28 11:24:33 -0500

    moved gitignore, added pa1

---
+++ Programming Assigment 1/Dalfonso_Luke_F2015_1428_1_PA1.cpp
@@ -0,0 +1,35 @@
+/*  Dalfonso_Luke_F2015_1428_1_PA1.cpp
+ *
+ *  Author: Luke D'Alfonso
+ *  Due Date: 9/2/2015
+ *  Programming Assignment Number 1
+ *  Fall 2015 - CS 1428 - Section Number 1
+ *
+ *  Instructor: Husain Gholoom.
+ *
+ *  *Prints data about me to stdout*
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+//FEAR THE MIGHTY BLOCK OF TEXT
+ cout << "***************************************************************************" << endl;
+ cout << "*  WELCOME                                                                *" << endl;
+ cout << "*                                                                         *" << endl;
+ cout << "*  My Name is : D'Alfonso, Luke J                                         *" << endl;
+ cout << "*  Date : 9/2/2015                                                        *" << endl;
+ cout << "*  Major : Computer Science                                               *" << endl;
+ cout << "*  Minor : None currently                                                 *" << endl;
+ cout << "*  Classification : Freshman                                              *" << endl;
+ cout << "*  About me :-                                                            *" << endl;
+ cout << "*     Programming skills : C++, Java, Python, JavaScript, Lua, VB         *" << endl;
+ cout << "*                                                                         *" << endl;
+ cout << "*     Favorite Movie : The Princess Bride                                 *" << endl;
+ cout << "*     Favorite Sport : Soccer                                             *" << endl;
+ cout << "*     Hobbies : Video games, programming/hacking, Magic the Gathering     *" << endl;
+ cout << "*                                                                         *" << endl;
+ cout << "***************************************************************************" << endl;
+ return 0;
+}
