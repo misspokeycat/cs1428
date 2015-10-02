6c2a9ec7e015845d1274bd278eb3a1c7c1f80b61
Luke D'Alfonso <lukedalfonso@yahoo.com>
2015-10-02 10:13:01 -0500

    Added prelab 6 solution

--- Lab 6 Prelab/lab_6_prelab.cpp
+++ Lab 6 Prelab/lab_6_prelab.cpp
@@ -1,7 +1,7 @@
 // Lab 6 Pre-Lab
 
-// Finish the following program which adds up all integers from 0 to 
-// the user's given number inclusively using a FOR loop. The total should be 
+// Finish the following program which adds up all integers from 0 to
+// the user's given number inclusively using a FOR loop. The total should be
 // assigned to the variable 'total'.
 
 #include <iostream>
@@ -16,14 +16,15 @@ int main()
     cout << "Enter a positive integer to find the summation of"
          << " all numbers from 0 to the given number." << endl;
     cin >> number;
-    
+
     // TODO - Add your code here
+    total = 0;
+    for (int x = 0; x <= number; x++) total += x ;
 
 
-    
 
 
     cout << "Total : " << total << endl;
-    
+
     return 0;
 }
